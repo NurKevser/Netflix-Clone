015e9524ac7f54c926093d11be76c79706212c5a
NurKevser <nurkevserkadiroglu@gmail.com>
2021-09-02 23:29:16 +0300

    created API_KEY fetched api

---
+++ src/requests.js
@@ -0,0 +1,12 @@
+const API_KEY = "6e2e71c5acfa466c4f0c394c317110c8";
+
+const request = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}$with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genre=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genre=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genre=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genre=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genre=99`,
+}
